ee2be2ece4cd6c9edbe6dca26ccf1b0e66ce19bb
Lalan Chaudhary <chaudharylalan28@gmail.com>
2025-07-02 01:48:44 +0530

    sfsd

--- controller/auth.js
+++ controller/auth.js
@@ -86,8 +86,8 @@ exports.adminLogin = async (req, res) => {
         const cookieOptions = {
             expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-            sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax'
+            secure: true,
+            sameSite:'none'
         };
 
         console.log("Cookie options:", cookieOptions);

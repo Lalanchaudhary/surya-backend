52676372cfe98cf6718f0ffb5a9858c87484a06b
Lalan Chaudhary <chaudharylalan28@gmail.com>
2025-07-04 00:38:49 +0530

    fgd

--- routes/Addons.js
+++ routes/Addons.js
@@ -5,7 +5,7 @@ const addonController = require('../controller/Addons');
 router.get('/addons', addonController.getAllAddons);
 router.get('/addons/:id', addonController.getAddonById);
 router.post('/addons', addonController.createAddon);
-router.put('/:id', addonController.updateAddon);
+router.put('/addons/:id', addonController.updateAddon);
 router.delete('/addons/:id', addonController.deleteAddon);
 
 module.exports = router;
